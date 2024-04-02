03042264ec3142d050e478cfe0d1eb0e83e860c1
Andre Beolchi <andregarbini2001@gmail.com>
2024-04-02 00:54:34 -0300

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,26 @@
+import nlwUniteIcon from "../assets/nlw-unite-icon.svg";
+
+export function Header() {
+	return (
+		<div className="flex items-center gap-5">
+			<img
+				src={nlwUniteIcon}
+				alt="NLW Unite"
+				className="w-12 h-12"
+			/>
+
+			<nav className="flex items-center gap-5">
+				<a
+					href="/"
+					className="font-medium text-sm text-zinc-300">
+					Eventos
+				</a>
+				<a
+					href="/"
+					className="font-medium text-sm">
+					Participantes
+				</a>
+			</nav>
+		</div>
+	);
+}
